316abab6a3f563519c06914d92df2731a9f49d00
Allison Harrison <allisonrharrison@gmail.com>
2020-07-30 20:45:00 -0700

    Updates

---
+++ precache-manifest.d11009a639d6be803b442acdaa5e3653.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24285810b2f21dc3745c2e7e5ae32783",
+    "url": "/nail-polish/index.html"
+  },
+  {
+    "revision": "537b763b03b9840aebb3",
+    "url": "/nail-polish/static/css/2.f4bde6ca.chunk.css"
+  },
+  {
+    "revision": "656273aca1dd7c224218",
+    "url": "/nail-polish/static/css/main.6e0501c9.chunk.css"
+  },
+  {
+    "revision": "537b763b03b9840aebb3",
+    "url": "/nail-polish/static/js/2.5c49cc12.chunk.js"
+  },
+  {
+    "revision": "26327434090b70d71b42b51eba5dbbdb",
+    "url": "/nail-polish/static/js/2.5c49cc12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "656273aca1dd7c224218",
+    "url": "/nail-polish/static/js/main.3b3bc8d2.chunk.js"
+  },
+  {
+    "revision": "55fbe55ec549002327da",
+    "url": "/nail-polish/static/js/runtime-main.22ba6766.js"
+  }
+]);
